42239d04a3ed3e5cb07de2d87d924b114a80599a
Dat Nguyen <datnguyen01122003@gmail.com>
2023-09-12 09:22:31 +0700

    update analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,11 @@ import "antd/dist/reset.css";
 import "./index.css";
 import "remixicon/fonts/remixicon.css";
 import App from "./App";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
